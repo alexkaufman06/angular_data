9fea9a0f2145eff2233c22cdcb44ae86a1333db0
Alex Kaufman <alexkaufman06@gmail.com>
2015-06-26 13:14:17 -0700

    Add user authentication

--- builds/development/js/controllers/registration.js
+++ builds/development/js/controllers/registration.js
@@ -1,7 +1,19 @@
-myApp.controller('RegistrationController', function($scope, $location) {
+myApp.controller('RegistrationController',
+  function($scope, $firebaseAuth, $location) {
+
+    var data = new Firebase('https://alexangulardata.firebaseio.com/');
+    var auth = $firebaseAuth(data);
+
   $scope.login = function() {
-    $location.path('/meetings');
-  } //login
+    auth.$authWithPassword({
+      email: $scope.user.email,
+      password: $scope.user.password
+    }).then(function(user) {
+      $location.path('/meetings');
+    }).catch(function(error) {
+      $scope.message = error.message;
+    });
+  }; //login
 
   $scope.register = function() {
     $location.path('/meetings');
